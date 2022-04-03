58fb490589091cf4d112d010148ade9da303ea4d
tomas <tomas@LAPTOP-160P5NS7>
2022-04-03 11:23:26 +0200

    Add project files.

---
+++ szyferApp/Program.cs
@@ -0,0 +1,30 @@
+﻿string text; //tekst do zaszyfrowania
+int key; //klucz - ilosc znakow przesuniecia
+
+
+string asciiCaesar(string t)
+{
+    t = t.ToUpper();
+    string encryptedText = ""; //zaszyfrowany tekst
+    for (int i = 0; i < t.Length; i++)
+    {
+        char letter = t[i];
+        int letterCode = (int)letter;
+        letterCode += key;
+        letterCode = ((letterCode - 65) % 26) + 65;
+        char encryptedLetter = (char)letterCode;
+        encryptedText += encryptedLetter;
+    }
+    return encryptedText;
+}
+
+Console.WriteLine("Podaj tekst do zaszyfrowania");
+text = Console.ReadLine() ?? "";
+Console.WriteLine("Podaj klucz (wartość przesunięcia):");
+key = int.Parse(Console.ReadLine() ?? "");
+
+
+
+Console.WriteLine("Szyfrowanie ASCII: " + asciiCaesar(text));
+
+
